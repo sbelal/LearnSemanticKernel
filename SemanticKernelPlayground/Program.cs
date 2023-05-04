8a15a9f427818a5506266387f5e8f5d9abf4e96a
Sal Belal <sbelal@gmail.com>
2023-05-05 00:17:26 +0100

    Add project files.

---
+++ SemanticKernelPlayground/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Enter some text:");
+        string? a = Console.ReadLine();
+        if (string.IsNullOrEmpty(a))
+        {
+            Console.WriteLine("EWQRQWERWQERQWE");
+        }
+    }
+}
